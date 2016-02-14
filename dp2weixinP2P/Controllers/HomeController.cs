b7e288b85508331019bbdca75d800bab80481272
jane <renyh1013@163.com>
2016-02-14 19:01:38 +0800

    增加功能页面

---
+++ dp2weixinP2P/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using DigitalPlatform.LibraryRestClient;
+using dp2weixin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dp2weixinP2P.Controllers
+{
+    public class HomeController : Controller
+    {
+        string url = "~/weixin/index";
+        /// <summary>
+        /// 通道所使用的 HTTP Cookies
+        /// </summary>
+        public CookieContainer Cookies = new System.Net.CookieContainer();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LibraryM()
+        {
+            return View();
+        }
+
+        //WeixinUser
+        public ActionResult WeixinUser()
+        {
+            return View();
+        }
+
+        //WeixinMessage
+        public ActionResult WeixinMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //WeixinMessage
+        public ActionResult WeixinMessagePost()
+        {
+            string msgSend = Request["txtMessage"];
+            string resultStr = "";
+            try
+            {
+                CookieAwareWebClient client = new CookieAwareWebClient(this.Cookies);
+                client.Headers["Content-type"] = "application/xml; charset=utf-8";
+                string xml = WeiXinClientUtil.GetPostXmlToWeiXinGZH(msgSend);
+                byte[] baData = Encoding.UTF8.GetBytes(xml);
+                byte[] result = client.UploadData(this.url,
+                    "POST",
+                    baData);
+                string strResult = Encoding.UTF8.GetString(result);
+                resultStr = strResult;
+
+                // 将焦点设回输入框
+                //this.lblMessage.Text = "您刚才发的消息是[" + this.txtMessage.Text + "]";
+
+                //this.txtMessage.Text = "";
+                //this.txtMessage.Focus();
+            }
+            catch (Exception ex)
+            {
+                resultStr = "Exception :" + ex.Message;
+            }
+
+            // 继续跟上登录成功返回的url
+            ViewBag.msgResult = resultStr;
+            return View();
+        }
+
+        //WeixinMenu
+        public ActionResult WeixinMenu()
+        {
+            return View();
+        }
+
+	}
+}
--- dp2weixinP2P/Controllers/WeixinController.cs
+++ dp2weixinP2P/Controllers/WeixinController.cs
@@ -32,9 +32,9 @@ namespace Senparc.Weixin.MP.Sample.Controllers
 
     public partial class WeixinController : Controller
     {
-        public static readonly string Token = "dp3weixin";//WebConfigurationManager.AppSettings["WeixinToken"];//与微信公众账号后台的Token设置保持一致，区分大小写。
-        public static readonly string EncodingAESKey = "ReQ72EHh7KkROvs1AEE5IK76py9oHmhRtVs30ur2DlD";// WebConfigurationManager.AppSettings["WeixinEncodingAESKey"];//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
-        public static readonly string AppId = "wx57aa3682c59d16c2";//WebConfigurationManager.AppSettings["WeixinAppId"];//与微信公众账号后台的AppId设置保持一致，区分大小写。
+        public static readonly string Token = WebConfigurationManager.AppSettings["WeixinToken"];//与微信公众账号后台的Token设置保持一致，区分大小写。
+        public static readonly string EncodingAESKey =  WebConfigurationManager.AppSettings["WeixinEncodingAESKey"];//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
+        public static readonly string AppId = WebConfigurationManager.AppSettings["WeixinAppId"];//与微信公众账号后台的AppId设置保持一致，区分大小写。
 
         readonly Func<string> _getRandomFileName = () => DateTime.Now.Ticks + Guid.NewGuid().ToString("n").Substring(0, 6);
 
