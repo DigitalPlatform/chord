f21cd216146dcaa8b08d5e35091d65d98027160f
XieTao <xietao@dp2003.com>
2016-04-22 09:49:30 +0800

    整理代码

--- DigitalPlatform.MessageServer/MyHub.cs
+++ DigitalPlatform.MessageServer/MyHub.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Microsoft.AspNet.SignalR;
 
 using DigitalPlatform.Message;
 using DigitalPlatform.Text;
-using System.Diagnostics;
 
 namespace DigitalPlatform.MessageServer
 {
