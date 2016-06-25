ba79b2c9647981104c31e9c2290f48aa947f06e8
renyh1013 <renyh1013@163.com>
2016-06-26 07:39:31 +0800

    修改书目检索界面样式

---
+++ dp2weixinP2P/Controllers/UIController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dp2weixinWeb.Controllers
+{
+    public class UIController : Controller
+    {
+        // GET: UI
+        public ActionResult BiblioIndex()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+    }
+}
